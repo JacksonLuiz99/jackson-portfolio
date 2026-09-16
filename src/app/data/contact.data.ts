821914b6bc875c4c5fce712512fc9838f5c42122
Jackson Luiz <jacksoncuiaba99@gmail.com>
2026-09-16 12:02:07 -0400

    fix: set real contact email

--- src/app/data/contact.data.ts
+++ src/app/data/contact.data.ts
@@ -1,5 +1,5 @@
 export const CONTACT = {
-  email: 'seu-email@exemplo.com',
+  email: 'jacksoncuiaba99@gmail.com',
   whatsapp: 'https://api.whatsapp.com/send?phone=5565999161859',
   linkedin: 'https://www.linkedin.com/in/jacksonluizdev/',
   github: 'https://github.com/JacksonLuiz99',
