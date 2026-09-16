ca72697d7cb1f3ca99841e0b71703574732b0cf1
Jackson Luiz <jacksoncuiaba99@gmail.com>
2026-09-16 11:27:00 -0400

    feat: assemble app shell composing all sections

---
+++ src/app/app.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar';
+import { HeroComponent } from './components/hero/hero';
+import { AboutComponent } from './components/about/about';
+import { ExperienceComponent } from './components/experience/experience';
+import { SkillsComponent } from './components/skills/skills';
+import { ProjectsComponent } from './components/projects/projects';
+import { EducationComponent } from './components/education/education';
+import { ContactComponent } from './components/contact/contact';
+import { FooterComponent } from './components/footer/footer';
+
+@Component({
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    AboutComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    EducationComponent,
+    ContactComponent,
+    FooterComponent,
+  ],
+  selector: 'app-root',
+  styleUrl: './app.scss',
+  templateUrl: './app.html',
+})
+export class App {}
